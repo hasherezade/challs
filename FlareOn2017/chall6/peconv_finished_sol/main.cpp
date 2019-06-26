8d30643e1644ac9923e6d80f974c5c0892b262e2
@hasherezade <hasherezade@users.noreply.github.com>
2019-06-26 20:36:46 +0200

    Update main.cpp

--- FlareOn2017/chall6/peconv_finished_sol/main.cpp
+++ FlareOn2017/chall6/peconv_finished_sol/main.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <windows.h>
-#include "peconv.h"
+#include <peconv.h>
 
 const size_t g_flagLen = 26;
 char g_flag[g_flagLen + 1] = { 0 };
@@ -19,15 +19,15 @@ int _stdcall my_MessageBoxA(
 {
     int key_part = 0;
     int key_id = 0;
-    sscanf(lpText,"key[%d] = %x;", &key_id, &key_part);
+    sscanf_s(lpText, "key[%d] = %x;", &key_id, &key_part);
     g_flag[key_id] = key_part;
     return 0;
 }
 
 DWORD exec_func(HMODULE loaded_pe, char *checked_str)
 {
-    DWORD (*exported_func) (DWORD arg0, DWORD arg1, LPSTR str, DWORD arg3) = NULL;
-    exported_func = (DWORD (*) (DWORD, DWORD, LPSTR, DWORD)) 
+    DWORD(*exported_func) (DWORD arg0, DWORD arg1, LPSTR str, DWORD arg3) = NULL;
+    exported_func = (DWORD(*) (DWORD, DWORD, LPSTR, DWORD))
         peconv::get_exported_func(loaded_pe, MAKEINTRESOURCE(1));
 
     if (exported_func == NULL) {
@@ -40,13 +40,13 @@ DWORD exec_func(HMODULE loaded_pe, char *checked_str)
 bool load_next_char(const char *path)
 {
     peconv::hooking_func_resolver my_res;
-    my_res.add_hook("MessageBoxA", (FARPROC) &my_MessageBoxA);
+    my_res.add_hook("MessageBoxA", (FARPROC)&my_MessageBoxA);
 
     size_t v_size = 0;
     BYTE* loaded_pe = peconv::load_pe_executable(
-        path, v_size, 
+        path, v_size,
         (peconv::t_function_resolver*) &my_res
-        );
+    );
 
     if (!loaded_pe) {
         printf("Loading module failed!\n");
@@ -61,8 +61,8 @@ bool load_next_char(const char *path)
     peconv::redirect_to_local64((void*)rand_offset, (ULONGLONG)&rand);
     peconv::redirect_to_local64((void*)calc_index_offset, (ULONGLONG)&my_index);
 
-    __int64 (__fastcall *to_overwrite_mem)(__int64 a1) = NULL;
-    to_overwrite_mem = ( __int64 (__fastcall *)(__int64 ))func_offset;
+    __int64(__fastcall *to_overwrite_mem)(__int64 a1) = NULL;
+    to_overwrite_mem = (__int64(__fastcall *)(__int64))func_offset;
     __int64 ret = to_overwrite_mem(0);
 
     std::vector<std::string> names_vec;
